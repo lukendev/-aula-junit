dad0ceea6a55b9ec0e8e08081b615aedda4861a0
lukendev <lucas.azevedo2104@gmail.com>
2022-09-22 15:02:59 -0300

    Second test, depositShouldDoNothingWhenNegativeAmount

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -8,7 +8,7 @@ import entities.Account;
 public class AccountTests {
 	
 	@Test
-	public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
 		
 		double amount = 200.0;
 		double expectedValue = 196.0;
@@ -17,5 +17,18 @@ public class AccountTests {
 		acc.deposit(amount);
 		
 		Assertions.assertEquals(expectedValue, acc.getBalance());
+	}
+	
+	@Test
+	public void depositShouldDoNothingWhenNegativeAmount() {
+		
+		double expectedValue = 100.0;
+		Account acc = new Account(1L, expectedValue);
+		double amount = -200.0;
+		
+		acc.deposit(amount);
+		
+		Assertions.assertEquals(expectedValue, acc.getBalance());
+		
 	}
 }
